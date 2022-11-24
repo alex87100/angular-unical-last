cd80393d2d938bfdc72a76e23fc65fd76b072fe6
Alex <alexcs@hotmail.it>
2022-11-24 01:08:59 +0100

    last

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardComponent} from './features/dashboard/dashboard.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {StudentModule} from "./features/student/student.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    StudentModule,
+    MatCardModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent
+  ],
+  exports: [
+    MatCardModule,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
